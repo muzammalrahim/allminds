a576acf72cd5bd36da4eb52138e122bc78f25024
Muzammal Rahim <muzammal.rahim@corptek.guru>
2019-11-07 20:08:18 +0500

    changes from local

---
+++ frontend/build/precache-manifest.96eeeddcb1e01cb7e06098423c14383a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe531106922052e6b83d81ceab6fd1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d1d9a14d68c146ede32",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5668e48448f7e25fd8e2",
+    "url": "/static/js/2.3db0da37.chunk.js"
+  },
+  {
+    "revision": "0d1d9a14d68c146ede32",
+    "url": "/static/js/main.2144f338.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  }
+]);
