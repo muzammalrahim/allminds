95d5617c1c634e117d57d3c49dd5301db027538a
Muzammal Rahim <muzammal.rahim@corptek.guru>
2019-11-12 17:37:29 +0500

    changes from local

--- import/settings.py
+++ import/settings.py
@@ -81,6 +81,7 @@ WSGI_APPLICATION = 'import.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
