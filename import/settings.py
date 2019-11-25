ce416d3cf389c7893a716238a88b2ef20d1b7d38
rabailr <51824550+rabailr@users.noreply.github.com>
2019-11-25 11:16:14 +0500

    Update settings.py

--- import/settings.py
+++ import/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'allminds',
     #     'USER': 'postgres',
-    #     'PASSWORD': '    ',
+    #     'PASSWORD': 'postgres',
     #     'HOST': 'localhost',
     #     'PORT': '5432',
     # }
