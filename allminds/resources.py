2086d879562e01f8756b1a6edea81aa7148129df
muzammalrahim <fahadali916@gmail.com>
2019-12-03 21:10:15 +0500

    remove duplicate

--- allminds/resources.py
+++ allminds/resources.py
@@ -1,37 +1,39 @@
 from import_export import resources
 from .models import Person
+from django.db import models
 
 class PersonResource(resources.ModelResource):
     # def before_import(self, dataset, using_transactions, dry_run, **kwargs):
     #     profileIdArray = []
     #     datasetNew = []
     #     tempDatasetList = list(dataset)
-    #     for index, data in enumerate(tempDatasetList, start=0):
-    #         url = list(data)[len(data)-2]
-    #         # print('data',dataset[index])
-    #         arrayIndex = index - 1
-    #         if(url != ''):
-    #             urlArray = url.strip().split("?")
-    #             urlArray = urlArray[0].strip().split("/")
-    #             i = len(urlArray) - 1
-    #             profile_id = urlArray[i]
-    #             # if profile_id not in profileIdArray:
-    #                 # profileIdArray.append(profile_id)
-    #             print("datasetttttttttttttttttt")
-    #             del tempDatasetList[arrayIndex]
-    #                 # dataset = dataset.pop(index)
-    #             print(profile_id,'index', arrayIndex)
-    #             # else:
-    #                 # datasetNew.append(dataset[index])
-    #                 # print(index,'no index')
-    #             # row.profile_id = profile_id
-    #             # print(dataset[index],'dataset[index]')
-    #             # print(profile_id,'profile_id')
-    #         # print(dataset,'dataset')
-    #     dataset = tuple(tempDatasetList)
-    #     # print(dataset,'dataset')
-    #     # print(tempDatasetList,'tempDatasetList')
-    #     return super(self.__class__, self).before_import(dataset, using_transactions, dry_run, **kwargs)
+    #     print(tempDatasetList,'tempDatasetList')
+        # for index, data in enumerate(tempDatasetList, start=0):
+        #     url = list(data)[len(data)-2]
+        #     # print('data',dataset[index])
+        #     arrayIndex = index - 1
+        #     if(url != ''):
+        #         urlArray = url.strip().split("?")
+        #         urlArray = urlArray[0].strip().split("/")
+        #         i = len(urlArray) - 1
+        #         profile_id = urlArray[i]
+        #         # if profile_id not in profileIdArray:
+        #             # profileIdArray.append(profile_id)
+        #         print("datasetttttttttttttttttt")
+        #         del tempDatasetList[arrayIndex]
+        #             # dataset = dataset.pop(index)
+        #         print(profile_id,'index', arrayIndex)
+        #         # else:
+        #             # datasetNew.append(dataset[index])
+        #             # print(index,'no index')
+        #         # row.profile_id = profile_id
+        #         # print(dataset[index],'dataset[index]')
+        #         # print(profile_id,'profile_id')
+        #     # print(dataset,'dataset')
+        # dataset = tuple(tempDatasetList)
+        # # print(dataset,'dataset')
+        # # print(tempDatasetList,'tempDatasetList')
+        # return super(self.__class__, self).before_import(dataset, using_transactions, dry_run, **kwargs)
 
     # def before_import_row(self, row, **kwargs):
     #     profile_id = None
@@ -45,10 +47,27 @@ class PersonResource(resources.ModelResource):
     #         print(profile_id,'profile_id')
     #     pass
 
+    def after_import(self, dataset, result, using_transactions, dry_run, **kwargs):
+        print('after_import')
+        # profiles = Person.objects.values_list('profile_id')
+        for row in Person.objects.all():
+            if Person.objects.filter(profile_id=row.profile_id).count() > 1:
+                row.delete()
+
     def before_save_instance(self, instance, using_transactions, dry_run):
         instance.cost_per_session_min = None
         instance.cost_per_session_max = None
-        years_in_practice_total = None
+        years_in_practice_total = None 
+        profile_id = None
+        
+        if(instance.url != ''):
+            urlArray = instance.url.strip().split("?")
+            urlArray = urlArray[0].strip().split("/")
+            index = len(urlArray) - 1
+            profile_id = urlArray[index]
+            instance.url = urlArray[0]
+            instance.profile_id = profile_id
+            # print(profile_id,'profile_id')
 
         if(instance.cost_per_session != 'N/A'):
             instance.cost_per_session_min = instance.cost_per_session.strip().replace("$","")
